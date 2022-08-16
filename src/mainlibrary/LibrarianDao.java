6344c3076489621d85a3cd155e2fee02898795d3
ajaykumbhar63 <111357394+ajaykumbhar63@users.noreply.github.com>
2022-08-16 18:13:07 +0100

    Add files via upload

---
+++ src/mainlibrary/LibrarianDao.java
@@ -0,0 +1,61 @@
+package mainlibrary;
+
+import java.sql.*;
+
+public class LibrarianDao {
+
+    public static int save(String name, String password, String email, String address, String city, String contact) {
+        int status = 0;
+        try {
+
+            Connection con = DB.getConnection();
+            PreparedStatement ps = con.prepareStatement("insert into librarian(name,password,email,address,city,contact) values(?,?,?,?,?,?)");
+            ps.setString(1, name);
+            ps.setString(2, password);
+            ps.setString(3, email);
+            ps.setString(4, address);
+            ps.setString(5, city);
+            ps.setString(6, contact);
+            status = ps.executeUpdate();
+            ps.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return status;
+    }
+
+    public static int delete(int id) {
+        int status = 0;
+        try {
+            Connection con = DB.getConnection();
+            PreparedStatement ps = con.prepareStatement("delete from Librarian where id=?");
+            ps.setInt(1, id);
+            status = ps.executeUpdate();
+            ps.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return status;
+    }
+
+    public static boolean validate(String name, String password) {
+        boolean status = false;
+        try {
+            Connection con = DB.getConnection();
+            String select = "select * from Librarian where UserName= ? and Password= ?";
+            PreparedStatement selectStatement = con.prepareStatement(select);
+            selectStatement.setString(1, name);
+            selectStatement.setString(2, password);
+            ResultSet rs = selectStatement.executeQuery();     
+            status = rs.next();
+            selectStatement.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return status;
+    }
+
+}
